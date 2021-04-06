f4e269f85afc5d2f81bc8d7e838312baaf669bad
Willdev <dimitriomorales01@gmail.com>
2021-04-06 09:22:43 +0100

    test zphisher

--- blurr-image/script.js
+++ blurr-image/script.js
@@ -1,17 +1,25 @@
 const bg = document.querySelector('.bg')
 const percentage = document.querySelector('.percentage')
 let blur = 100 
+let load = 0
 
 var idVar = setInterval(() => {
     if(blur != 0) {
         blur--
+        load++
         bg.style.filter = "blur("+blur.toString()  + "px)"
-        percentage.innerHTML = blur.toString() + "%"
+        percentage.innerHTML = scale(blur, 0, 100, 1, 0) + "%"
+        // percentage.style.opacity = scale(load, 0, 100, 0, 1)
     } else {
         myStopFunction()
+        percentage.style.opacity = 0
     }
 }, 30);
  
 function myStopFunction() {
     clearInterval(idVar);
 }
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+  }
